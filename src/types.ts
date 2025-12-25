e8dd37ece820a3192b23534e91279d22a49a8a54
Ramirez Vargas, José Pablo <webJose@gmail.com>
2025-12-25 17:46:23 -0600

    chore(jsdocs): Complete JsDoc's for all types

--- src/types.ts
+++ src/types.ts
@@ -20,12 +20,34 @@ export interface ImportMap {
     integrity?: Record<string, string>;
 }
 
+/**
+ * Defines the data output from validating an import map.
+ */
 export interface ValidationResult {
+    /**
+     * Indicates whether the import map is valid.
+     */
     readonly valid: boolean;
+    /**
+     * List of validation error messages.
+     */
     readonly errors: string[];
 }
 
+/**
+ * Defines the capabilities provided by resolver objects.
+ */
 export interface Resolver {
+    /**
+     * Resolves a module specifier using the contained import map.
+     * 
+     * **IMPORTANT**:  Resolution cannot take place against scoped mappings without an importer.
+     * @param specifier Module specifier to resolve.
+     * @param importer Optional importer value.
+     * @returns Resolved module specifier if a match was found; `undefined` if the input identifier 
+     * was a bare module identifier; the `specifier` when no match was found but it is a relative, 
+     * absolute or full URL.
+     */
     resolve(specifier: string, importer?: string | undefined): string | undefined;
     readonly valid: boolean;
     readonly validationResult: ValidationResult;
